9ac3c4402f5ec429a916c19165fc8d0a5f91dcca
Michael LaDouceur <michaelladouceur1@gmail.com>
2019-07-22 06:56:51 -0500

    Single Select is almost completed; Minor tweaks left

--- cli.py
+++ cli.py
@@ -14,13 +14,14 @@ class SSelect:
         message = None,
         choices = [],
         max_lines = None,
-        message_orient = 'TOP',
-        choice_orient_x = 'CENTER',
-        choice_orient_y = 'CENTER',
+        message_orient = 'top',
+        choice_orient_x = 'center',
+        choice_orient_y = 'center',
         word_color = colors.colors['defaultW'],
         word_select_color = colors.colors['black'],
         back_color = colors.colors['black'],
-        back_select_color = colors.colors['defaultSB']
+        back_select_color = colors.colors['defaultSB'],
+        menu_color = colors.colors['defaultW']
         ):
 
         if not choices:
@@ -30,7 +31,16 @@ class SSelect:
 
         self.message = message
         self.choices = choices
-        self.max_lines = min(self.h, max_lines)
+        if self.message is not None:
+            if isinstance(choice_orient_y, int):
+                self.max_lines = min(self.h-choice_orient_y, max_lines-choice_orient_y)
+            else:
+                self.max_lines = min(self.h, max_lines)
+        else:
+            if isinstance(choice_orient_y, int):
+                self.max_lines = min(self.h-choice_orient_y, max_lines-choice_orient_y)  
+            else:
+                self.max_lines = min(self.h, max_lines)
         self.offset = 0
         self.selected = 0
 
@@ -42,12 +52,14 @@ class SSelect:
         self.word_select_color = self.map_colors(word_select_color)
         self.back_color = self.map_colors(back_color)
         self.back_select_color = self.map_colors(back_select_color)
+        self.menu_color = self.map_colors(menu_color)
 
         self.init_colors(
             *self.word_color, 
             *self.back_color,
             *self.word_select_color,
-            *self.back_select_color)
+            *self.back_select_color,
+            *self.menu_color)
 
     def init_screen(self):
         self.screen = curses.initscr()
@@ -65,9 +77,10 @@ class SSelect:
         r1,g1,b1,
         r2,g2,b2,
         r3,g3,b3,
-        r4,g4,b4):
+        r4,g4,b4,
+        r5,g5,b5):
         
-        # Display colors
+        # Default colors
         curses.init_color(1,r1,g1,b1)
         curses.init_color(2,r2,g2,b2)
         curses.init_pair(1,1,2)
@@ -76,6 +89,12 @@ class SSelect:
         curses.init_color(3,r3,g3,b3)
         curses.init_color(4,r4,g4,b4)
         curses.init_pair(2,3,4)
+
+        # Menu colors
+        curses.init_color(5,r5,g5,b5)
+        curses.init_pair(3,5,2)
+
+        self.screen.bkgd(' ', curses.color_pair(1))
 
     def map_colors(self,color):
         return tuple(map(lambda x: math.ceil(x*3.92156), color))
@@ -91,10 +110,12 @@ class SSelect:
 
     def orient_x(self, item):
         try:
-            if self.choice_orient_x == 'CENTER':
+            if self.choice_orient_x == 'center':
                 x = self.w//2 - int(len(str(item))//2)
-            elif self.choice_orient_x == 'LEFT':
+            elif self.choice_orient_x == 'left':
                 x = 0
+            elif isinstance(self.choice_orient_x, int):
+                x = self.choice_orient_x
         except:
             print(f'choice_orient_x can not equal that')
 
@@ -102,17 +123,19 @@ class SSelect:
 
     def orient_y(self, idx):
         try:
-            if self.choice_orient_y == 'CENTER':
+            if self.choice_orient_y == 'center':
                 y = (self.h//2 - len(self.displayItems)//2) + idx
-            elif self.choice_orient_y == 'TOP':
+            elif self.choice_orient_y == 'top':
                 y = idx
+            elif isinstance(self.choice_orient_y, int):
+                y = self.choice_orient_y + idx
         except:
             print(f'choice_orient_y can not equal that')
 
         return y
 
     def orient_message(self, x, y):
-        if self.message_orient == 'TOP':
+        if self.message_orient == 'top':
             mx = self.w//2 - int(len(self.message)//2)
             my = y - 2
 
@@ -143,8 +166,10 @@ class SSelect:
     def display_message(self, x, y):
         if self.message is not None:
             mx, my = self.orient_message(x, y)
-            self.screen.addstr(my,mx,self.message, curses.color_pair(1))
-            self.screen.addstr(my+1,mx,'='*len(self.message), curses.color_pair(1))
+            totalPage = math.ceil(len(self.choices)/self.max_lines)
+            currentPage = math.ceil(self.offset/self.max_lines) + 1
+            self.screen.addstr(my,mx,f'{self.message} ({currentPage} of {totalPage})', curses.color_pair(3))
+            self.screen.addstr(my+1,mx,'='*len(self.message), curses.color_pair(3))
         else:
             return
 
@@ -185,15 +210,24 @@ class SSelect:
                 self.page(self.DOWN)
             elif input == curses.KEY_LEFT:
                 self.page(self.UP)
-            elif input == 10 or 459:
+            elif input == 10:
+                self.deinit_screen()
                 return self.choices[self.selected+self.offset]
             elif input == 27:
+                self.deinit_screen()
                 break
+            else:
+                pass
 
 select = SSelect(
-    message = 'MAIN MENU',
-    choices=[x for x in range(0,100)],
-    word_color=(144, 252, 202), max_lines=20)
+    message = 'PART NUMBER',
+    choices=[f'PPC{random.randrange(100000)}.40' for i in range(100)],
+    menu_color=colors.colors['blue'], max_lines=30)
 answer = select.run()
 print(answer)
-select.deinit_screen()
+# select = SSelect(
+#     message = 'WORK CENTER ID',
+#     choices=[f'{random.randrange(9999)}' for i in range(20)],
+#     menu_color=(144, 252, 202), max_lines=10)
+# answer = select.run()
+# print(answer)
