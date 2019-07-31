d32466ceac0e94eaade56c674b70035a6484cd11
Michael LaDouceur <michaelladouceur1@gmail.com>
2019-07-30 23:48:02 -0500

    Refactor to ListComponent class

--- component.py
+++ component.py
@@ -1,3 +1,6 @@
+import curses
+from curses.textpad import rectangle
+import math
 from screen import init_screen, init_colors, deinit_screen
 from colors import colors
 
@@ -7,12 +10,12 @@ class Component():
 
     def __init__(
         self,
-        message = None,
-        word_color = colors['white'],
-        word_select_color = colors['charcoal'],
-        back_color = colors['charcoal'],
-        back_select_color = colors['white'],
-        message_color = colors['white']
+        message,
+        word_color,
+        word_select_color,
+        back_color,
+        back_select_color,
+        message_color
         ):
 
         self.screen, self.h, self.w = init_screen()
@@ -36,3 +39,146 @@ class Component():
 
     def set_message_height(self):
         self.message_height = 3 if self.message is not None else 0
+
+    def display_message(self, x, y):
+        if self.message is not None:
+            mx, my = self.orient_message(x, y)
+            totalPage = math.ceil(len(self.choices)/self.max_lines)
+            currentPage = math.ceil(self.offset/self.max_lines) + 1
+            self.screen.addstr(my,mx,f'{self.message}', curses.color_pair(3))
+            self.screen.addstr(my+1,mx+1,f'({currentPage} of {totalPage})', curses.color_pair(3))
+            rectangle(self.screen, my-1, mx-1, my+2, mx+len(self.message))
+        else:
+            return
+
+class ListComponent(Component):
+
+    def __init__(
+        self,
+        message,
+        choices,
+        max_lines,
+        x_position,
+        y_position,
+        word_color,
+        word_select_color,
+        back_color,
+        back_select_color,
+        message_color
+        ):
+
+        super().__init__(
+            message,
+            word_color,
+            word_select_color,
+            back_color,
+            back_select_color,
+            message_color
+        )
+
+        if not choices:
+            raise ValueError('Choices can not be empty')
+
+        self.choices = choices
+
+        self.max_lines = max_lines
+        self.x_position = x_position
+        self.y_position = y_position
+        self.set_prompt_boundaries()
+
+        self.offset = 0
+        self.selected = 0
+
+    def set_prompt_boundaries(self):
+        ''' Set prompt height '''
+        if self.max_lines is not None:
+            self.prompt_height = min(
+                self.max_lines + self.message_height, 
+                len(self.choices) + self.message_height, 
+                self.h - 1)
+        else:
+            self.prompt_height = min(
+                len(self.choices) + self.message_height, 
+                self.h - 1)
+
+        ''' Set prompt top '''
+        if self.y_position == 'center':
+            self.prompt_top = self.h//2 - self.prompt_height//2
+        elif self.y_position == 'top':
+            self.prompt_top = 0
+        elif isinstance(self.y_position, int):
+            self.prompt_height -= self.y_position
+            self.prompt_top = self.y_position
+
+        ''' Set prompt bottom '''
+        self.prompt_bottom = self.prompt_top + self.prompt_height
+
+        ''' Set max lines for choices '''
+        self.max_lines = self.prompt_height - self.message_height
+        self.choices_top = self.prompt_top + self.message_height
+
+    def orient_x(self, item):
+        try:
+            if self.x_position == 'center':
+                x = self.w//2 - int(len(str(item))//2)
+            elif self.x_position == 'left':
+                x = 0
+            elif isinstance(self.x_position, int):
+                x = self.x_position
+        except:
+            print(f'x_position can not equal that')
+
+        return x
+
+    def orient_y(self, idx):
+        return self.choices_top + idx
+
+    def orient_message(self, x, y):
+        mx = self.w//2 - int(len(self.message)//2)
+        my = self.prompt_top
+
+        return mx, my
+
+    def scroll(self, direction):
+        if self.selected == 0 and direction == self.UP:
+            self.selected = len(self.displayItems) - 1
+        elif self.selected == len(self.displayItems) - 1 and direction == self.DOWN:
+            self.selected = 0
+        else:
+            self.selected -= direction
+
+    def page(self, direction):
+        offTest = self.offset
+        if direction == self.UP:
+            offTest -= self.max_lines
+        elif direction == self.DOWN:
+            offTest += self.max_lines
+        else:
+            return
+
+        if 0 <= offTest < len(self.choices):
+            self.offset = offTest
+        else:
+            return
+
+    def display(self, idx, item):
+        x = self.orient_x(item)
+        y = self.orient_y(idx)
+        if idx == 0:
+            self.display_message(x,y)
+
+        if idx == self.selected:
+            self.screen.addstr(y,x,str(item),curses.color_pair(2))
+        else:
+            self.screen.addstr(y,x,str(item),curses.color_pair(1))
+
+    def render(self):
+        self.screen.erase()
+        if self.max_lines is not None:
+            self.displayItems = self.choices[self.offset:(self.max_lines+self.offset)]
+        else:
+            self.displayItems = self.choices
+
+        for idx, item in enumerate(self.displayItems):
+            self.display(idx, item)
+        self.screen.refresh()
--- sselect.py
+++ sselect.py
@@ -5,11 +5,11 @@ import math
 import random
 from colors import colors
 from screen import init_screen, init_colors, deinit_screen
-from component import Component
+from component import Component, ListComponent
 
 ''' Single Selection Component: Select one item from choices to return '''
 
-class SSelect(Component):
+class SSelect(ListComponent):
 
     UP = 1
     DOWN = -1
@@ -30,138 +30,16 @@ class SSelect(Component):
 
         super().__init__(
             message,
+            choices,
+            max_lines,
+            x_position,
+            y_position,
             word_color, 
             word_select_color, 
             back_color, 
             back_select_color, 
             message_color
             )
-
-        if not choices:
-            raise ValueError('Choices can not be empty')
-        # if not isinstance(y_position, int):
-        #     raise ValueError('y_position can not be less than 0')
-        # elif 
-        # if x_position < 0:
-        #     raise ValueError('x_position can not be less than 0')
-
-        
-        self.choices = choices
-
-        self.max_lines = max_lines
-        self.x_position = x_position
-        self.y_position = y_position
-        self.set_prompt_boundaries()
-        
-        self.offset = 0
-        self.selected = 0
-        self.data = []
-
-    def set_prompt_boundaries(self):
-        ''' Set prompt height '''
-        if self.max_lines is not None:
-            self.prompt_height = min(
-                self.max_lines + self.message_height, 
-                len(self.choices) + self.message_height, 
-                self.h - 1)
-        else:
-            self.prompt_height = min(
-                len(self.choices) + self.message_height, 
-                self.h - 1)
-
-        ''' Set prompt top '''
-        if self.y_position == 'center':
-            self.prompt_top = self.h//2 - self.prompt_height//2
-        elif self.y_position == 'top':
-            self.prompt_top = 0
-        elif isinstance(self.y_position, int):
-            self.prompt_height -= self.y_position
-            self.prompt_top = self.y_position
-
-        ''' Set prompt bottom '''
-        self.prompt_bottom = self.prompt_top + self.prompt_height
-
-        ''' Set max lines for choices '''
-        self.max_lines = self.prompt_height - self.message_height
-        self.choices_top = self.prompt_top + self.message_height
-
-    def orient_x(self, item):
-        try:
-            if self.x_position == 'center':
-                x = self.w//2 - int(len(str(item))//2)
-            elif self.x_position == 'left':
-                x = 0
-            elif isinstance(self.x_position, int):
-                x = self.x_position
-        except:
-            print(f'x_position can not equal that')
-
-        return x
-
-    def orient_y(self, idx):
-        return self.choices_top + idx
-
-    def orient_message(self, x, y):
-        mx = self.w//2 - int(len(self.message)//2)
-        my = self.prompt_top
-
-        return mx, my
-
-    def scroll(self, direction):
-        if self.selected == 0 and direction == self.UP:
-            self.selected = len(self.displayItems) - 1
-        elif self.selected == len(self.displayItems) - 1 and direction == self.DOWN:
-            self.selected = 0
-        else:
-            self.selected -= direction
-
-    def page(self, direction):
-        offTest = self.offset
-        if direction == self.UP:
-            offTest -= self.max_lines
-        elif direction == self.DOWN:
-            offTest += self.max_lines
-        else:
-            return
-
-        if 0 <= offTest < len(self.choices):
-            self.offset = offTest
-        else:
-            return
-    
-    def display_message(self, x, y):
-        if self.message is not None:
-            mx, my = self.orient_message(x, y)
-            totalPage = math.ceil(len(self.choices)/self.max_lines)
-            currentPage = math.ceil(self.offset/self.max_lines) + 1
-            self.screen.addstr(my,mx,f'{self.message}', curses.color_pair(3))
-            self.screen.addstr(my+1,mx+1,f'({currentPage} of {totalPage})', curses.color_pair(3))
-            rectangle(self.screen, my-1, mx-1, my+2, mx+len(self.message))
-        else:
-            return
-
-
-    def display(self, idx, item):
-        x = self.orient_x(item)
-        y = self.orient_y(idx)
-        if idx == 0:
-            self.display_message(x,y)
-
-        if idx == self.selected:
-            self.screen.addstr(y,x,str(item),curses.color_pair(2))
-        else:
-            self.screen.addstr(y,x,str(item),curses.color_pair(1))
-
-    def render(self):
-        self.screen.erase()
-        if self.max_lines is not None:
-            self.displayItems = self.choices[self.offset:(self.max_lines+self.offset)]
-        else:
-            self.displayItems = self.choices
-
-        for idx, item in enumerate(self.displayItems):
-            self.display(idx, item)
-        self.screen.refresh()
 
     def run(self):
 
@@ -188,13 +66,13 @@ class SSelect(Component):
 
 select = SSelect(
     message = 'PART NUMBER',
-    choices=[f'PPC{random.randrange(100000)}.40' for i in range(10000)], max_lines=15)
+    choices=[f'PPC{random.randrange(100000)}.40' for i in range(100)], y_position=5, max_lines=12)
 answer = select.run()
 print(answer)
 
 select = SSelect(
-    # message = 'PART NUMBER',
-    choices=[f'PPC{random.randrange(100000)}.40' for i in range(100)], max_lines=10)
+    message = 'PART NUMBER',
+    choices=[f'PPC{random.randrange(100000)}.40' for i in range(100)], y_position=5, max_lines=12)
 answer = select.run()
 print(answer)
 
